47b4c6cb2740615f8e8d606d93a151f01db3830d
Nivaash <nidishnivash@gmail.com>
2024-02-14 15:10:52 +0530

    data is successfully fetched from the database

--- Backend/Routers.js
+++ Backend/Routers.js
@@ -25,51 +25,6 @@ router.get("/", async (req,res) => {
     })
 })
 
-// router.post("/", async (req,res) => {
-//     const newdata = new Song(req.body)
-//     await newdata.save().then((result) => {
-//         res.send("New Song Added!!!")
-//     }).catch((err) => {
-//         res.status(500).send(err)
-//     })
-// })
-
-// router.put("/:SongId", async (req,res)=> {
-//     try{
-//         let {SongId} = req.params
-//         let newData = req.body
-
-//         let result  = await Song.findOneAndUpdate({SongId: SongId}, newData)
-
-//         if (result === null || result === undefined){
-//             res.status(404).send(err)
-//         }
-//         else{
-//             res.send("Updated!!!")
-//         }c d
-//     }catch(err){
-//         res.status(500).send("Error!!!: ",err.message)
-//     }
-// })
-
-// router.delete("/", async (req,res) => {
-//     let deleteSong = req.body.title
-
-//     try{
-//         let result = await Song.deleteOne({title: deleteSong})
-
-//         if(result.deletedCount == 0){
-//             res.status(404).send("Song not found!!!!")
-//         }
-//         else{
-//             res.send("Song Deleted!!!")
-//         }
-//     }catch(err){
-//         res.status(500).send("Error Deleting Song",err.message)
-//     }
-
-
-// })
 
 module.exports= {
     router
