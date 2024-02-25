c7f047105a2d638215c6944f3867e2a1345eed4b
Nivaash <nidishnivash@gmail.com>
2024-02-25 14:07:15 +0530

    done

--- Backend/server.js
+++ Backend/server.js
@@ -1,15 +1,13 @@
 // server.js
 
 const express = require("express");
-const { MongoClient } = require("mongodb");
-var cors = require('cors')
-const app = express();
-app.use(cors())
-app.use(express.json());
-const { signUpRouter, songRouter , editRouter , deleteRouter, postRouter } = require("./Routers")
-const mongoose = require("mongoose")
-require("dotenv").config()
+const { songRouter, signUpRouter, editRouter, deleteRouter, postRouter } = require("./Routers");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
+const app = express();
+
+// Connect to MongoDB
 async function connectDatabase() {
     try {
         await mongoose.connect(process.env.mongoUrl);
@@ -19,33 +17,39 @@ async function connectDatabase() {
     }
 }
 
+// Middleware for CORS and JSON parsing
+app.use(cors());
+app.use(express.json());
+
+// Routes
 app.get("/ping", (req, res) => {
     res.send("Hi");
 });
 
+// Connect to database route
 app.get("/", (req, res) => {
     connectDatabase()
         .then(() => {
             console.log('Connected to Database!!!')
-        })
+        });
     res.status(200).send("Connected to Database!!!")
-    });
+});
 
-app.use("/songs", songRouter)
-app.use("/signup", signUpRouter) 
-app.use("/edit",editRouter)
-app.use("/delete",deleteRouter)
-app.use("/post",postRouter)
+// Routes using the routers
+app.use("/songs", songRouter);
+app.use("/signup", signUpRouter);
+app.use("/edit", editRouter);
+app.use("/delete", deleteRouter);
+app.use("/post", postRouter);
 
-
+// Error handling middleware
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send("Something went wrong!");
 });
 
-
+// Start the server
 const PORT = process.env.PORT || 3000;
-
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
